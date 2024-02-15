acb8faa1fc45b75e616db564a9fcaf6c79566e82
denisdelaki <denisdelaki@gmail.com>
2024-02-15 17:06:10 +0300

    created the components for the project

---
+++ src/app/view-recipes/view-recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-recipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-recipes.component.html',
+  styleUrl: './view-recipes.component.sass'
+})
+export class ViewRecipesComponent {
+
+}
