73a4c4a6acd8d51ab7ce461459fca1f52f75969d
denisdelaki <denisdelaki@gmail.com>
2024-02-17 20:11:14 +0300

    created the card in the landing page body added styling properties

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatMenuModule,MatToolbarModule],
+  imports: [MatMenuModule,MatToolbarModule, MatCardModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
