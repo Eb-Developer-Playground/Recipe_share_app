9bc14453c45a77dde6f3f16c7a93355d105ef343
denisdelaki <denisdelaki@gmail.com>
2024-02-17 16:45:13 +0300

    created the header menus and style it to enhance the landing page UI

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule,MatToolbarModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.sass'
+  styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
