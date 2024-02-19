c15a7a7153246ac50b50d344c1a83cfa89ea9b90
denisdelaki <denisdelaki@gmail.com>
2024-02-19 13:01:38 +0300

    created the About us section and styled the properties in column form to display key features about Equity Eateries

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
@@ -6,10 +7,17 @@ import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatMenuModule,MatToolbarModule, MatCardModule,MatIconModule],
+  imports: [CommonModule, MatMenuModule,MatToolbarModule, MatCardModule,MatIconModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  showContent = false;
 
+  ngOnInit(): void {
+    // Set a timeout to show the content after 3000 milliseconds (3 seconds)
+    setTimeout(() => {
+      this.showContent = true;
+    }, 3000);
+  }
 }
