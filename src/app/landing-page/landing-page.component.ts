3239fd59b7d54af53591fe2f096815a0366b31c7
denisdelaki <denisdelaki@gmail.com>
2024-02-19 11:38:07 +0300

    created the review and testimonials sections and applied styling properties

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatMenuModule,MatToolbarModule, MatCardModule],
+  imports: [MatMenuModule,MatToolbarModule, MatCardModule,MatIconModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
