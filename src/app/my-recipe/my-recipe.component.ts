a2d28ee7096c9e6c286e2f358579a6ce96250413
denisdelaki <denisdelaki@gmail.com>
2024-02-22 09:03:30 +0300

    adjusted the styling properties for the naviagtion section

--- src/app/my-recipe/my-recipe.component.ts
+++ src/app/my-recipe/my-recipe.component.ts
@@ -4,10 +4,11 @@ import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-my-recipe',
   standalone: true,
-  imports: [RouterModule,MatMenuModule,MatToolbarModule,MatCardModule,MatFormFieldModule],
+  imports: [RouterModule,MatMenuModule,MatToolbarModule,MatCardModule,MatFormFieldModule,MatIconModule],
   templateUrl: './my-recipe.component.html',
   styleUrl: './my-recipe.component.scss'
 })
