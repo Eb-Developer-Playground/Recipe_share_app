a0e04dd2fa5fc08f9fe0c77603deb22eec466648
denisdelaki <denisdelaki@gmail.com>
2024-02-26 13:06:18 +0300

    populate the edit form with existing data

--- src/app/edit-recipe/edit-recipe.component.ts
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import { MatFormFieldModule} from '@angular/material/form-field';
@@ -10,7 +10,9 @@ import { RecipesData } from '../sign-up/recipes';
 import { MatSelectModule} from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router,ActivatedRoute } from '@angular/router';
+
+
 @Component({
   selector: 'app-edit-recipe',
   standalone: true,
@@ -24,15 +26,12 @@ import { Router } from '@angular/router';
   templateUrl: './edit-recipe.component.html',
   styleUrl: './edit-recipe.component.scss'
 })
-export class EditRecipeComponent {
-Edit() {
-throw new Error('Method not implemented.');
-}
-close() {
-throw new Error('Method not implemented.');
-}
-editrecipeForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder, private recipeservice: RecipesService, private router:Router) { }
+export class EditRecipeComponent implements OnInit {
+  recipeId: any;
+  constructor(private formBuilder: FormBuilder, 
+    private recipeservice: RecipesService,
+    private route: ActivatedRoute,
+    private router:Router) { }
   //category optons 
 Categorys = [
   {value: 'BreakFast-0', viewValue: 'BreakFast'},
@@ -40,4 +39,44 @@ Categorys = [
   {value: 'Dinner-2', viewValue: 'Dinner'},
   {value: 'Snack-3', viewValue: 'Snack'}
 ];
+//form initialization 
+  editrecipeForm!:FormGroup;
+ngOnInit(): void {
+  this.editrecipeForm = this.formBuilder.group({
+    title: [''],
+    category: [''],
+    ingredients: [''],
+    instruction: [''],
+    recipimgurl: ['']
+  });
+  // Retrieve recipe ID from route parameters
+  this.route.params.subscribe(params => {
+    this.recipeId = params['id'];
+    // Fetch recipe data using this.recipeId and populate form fields
+    this.fetchRecipeData(this.recipeId);
+  });
+}
+
+fetchRecipeData(recipeId: any) {
+ //fetch recipe data from service based on recipeId
+  this.recipeservice.getData(recipeId).subscribe(recipe => {
+    // Populate form fields with fetched data
+    this.editrecipeForm.patchValue({
+      title: recipe.title,
+      category: recipe.category,
+      ingredients: recipe.ingredients,
+      instruction: recipe.instruction,
+      recipimgurl: recipe.recipimgurl
+    });
+  });
+}
+
+
+  
+Edit() {
+  
+  }
+  close() {
+  throw new Error('Method not implemented.');
+  }
 }
